69945f28b771ae19c8052b167a1158af330c4d4a
Kamyar Lajani <kamolajani1@gmail.com>
2020-11-24 23:36:26 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export { default as Message } from './Message.svelte';
 export { default as AllMessages } from './AllMessages.svelte';
-export { isInvalid } from './isValid.js';
+export { isInvalid } from './isinvalid.js';
