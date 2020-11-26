717e3f4ecc1385680c3332995f110c7c5d73256a
Kamyar Lajani <kamolajani1@gmail.com>
2020-11-26 20:51:04 +0300

    initial commit

--- src/validate.js
+++
@@ -1,9 +0,0 @@
-export let validate = (schema, fields)=>{
-    try {
-        schema.validateSync(fields, {abortEarly: false});
-    }
-    catch(error){
-        return error.inner;
-    }
-    return [];
-}
